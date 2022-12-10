8e10e6c587619fdf972a671aaab59916af3c28a6
Naufal Sidik <sidiknaufal127@gmail.com>
2022-12-10 07:48:28 +0700

    edit

--- App/Pages/HomeScreen/index.js
+++ App/Pages/HomeScreen/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, StatusBar, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Text, View, StatusBar, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { IcLonceng, IcCheckBox } from '../../assets';
 
 export default HomeScreen = () => {
@@ -41,57 +41,6 @@ export default HomeScreen = () => {
 
       <View style={styles.body}>
           <Text style={styles.today}>Today's Task</Text>
-        <View>
-
-          <View style={styles.body1}>
-            <View styles={styles.jadwal}>
-              <Text style={styles.jam}>10:00</Text>
-              <Image style={styles.checkbox} source={IcCheckBox}/>
-            </View>
-            <View style={styles.Container}>
-              <Text style={styles.M}>M</Text>
-              <Text style={styles.meeting}>Meeting With Ceo</Text>
-              <Text style={styles.line}>────────────────</Text>
-            </View>
-          </View>
-
-          <View style={styles.body2}>
-            <View styles={styles.jadwal}>
-              <Text style={styles.jam}>10:00</Text>
-              <Image style={styles.checkbox} source={IcCheckBox}/>
-            </View>
-            <View style={styles.Container}>
-              <Text style={styles.M}>M</Text>
-              <Text style={styles.meeting}>Meeting With Ceo</Text>
-              <Text style={styles.line}>────────────────</Text>
-            </View>
-          </View>
-
-          <View style={styles.body3}>
-            <View styles={styles.jadwal}>
-              <Text style={styles.jam}>10:00</Text>
-              <Image style={styles.checkbox} source={IcCheckBox}/>
-            </View>
-            <View style={styles.Container}>
-              <Text style={styles.M}>M</Text>
-              <Text style={styles.meeting}>Meeting With Ceo</Text>
-              <Text style={styles.line}>────────────────</Text>
-            </View>
-          </View>
-
-          <View style={styles.body4}>
-            <View styles={styles.jadwal}>
-              <Text style={styles.jam}>10:00</Text>
-              <Image style={styles.checkbox} source={IcCheckBox}/>
-            </View>
-            <View style={styles.Container}>
-              <Text style={styles.M}>M</Text>
-              <Text style={styles.meeting}>Meeting With Ceo</Text>
-              <Text style={styles.line}>────────────────</Text>
-            </View>
-          </View>
-
-        </View>
       </View>
     </View>
   );
--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 TaskitaExpo Kesit Wijanarko
+
+<View>
+
+          <View style={styles.body1}>
+            <View styles={styles.jadwal}>
+              <Text style={styles.jam}>10:00</Text>
+              <Image style={styles.checkbox} source={IcCheckBox}/>
+            </View>
+            <View style={styles.Container}>
+              <Text style={styles.M}>M</Text>
+              <Text style={styles.meeting}>Meeting With Ceo</Text>
+              <Text style={styles.line}>────────────────</Text>
+            </View>
+          </View>
+
+          <View style={styles.body2}>
+            <View styles={styles.jadwal}>
+              <Text style={styles.jam}>10:00</Text>
+              <Image style={styles.checkbox} source={IcCheckBox}/>
+            </View>
+            <View style={styles.Container}>
+              <Text style={styles.M}>M</Text>
+              <Text style={styles.meeting}>Meeting With Ceo</Text>
+              <Text style={styles.line}>────────────────</Text>
+            </View>
+          </View>
+
+          <View style={styles.body3}>
+            <View styles={styles.jadwal}>
+              <Text style={styles.jam}>10:00</Text>
+              <Image style={styles.checkbox} source={IcCheckBox}/>
+            </View>
+            <View style={styles.Container}>
+              <Text style={styles.M}>M</Text>
+              <Text style={styles.meeting}>Meeting With Ceo</Text>
+              <Text style={styles.line}>────────────────</Text>
+            </View>
+          </View>
+
+          <View style={styles.body4}>
+            <View styles={styles.jadwal}>
+              <Text style={styles.jam}>10:00</Text>
+              <Image style={styles.checkbox} source={IcCheckBox}/>
+            </View>
+            <View style={styles.Container}>
+              <Text style={styles.M}>M</Text>
+              <Text style={styles.meeting}>Meeting With Ceo</Text>
+              <Text style={styles.line}>────────────────</Text>
+            </View>
+          </View>
+
+        </View>
